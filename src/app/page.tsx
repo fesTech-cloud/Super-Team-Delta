2fd74a851f18513105d79e23cdf60409aba22768
Ovie Nathaniel <ovienathaniel44@gmail.com>
2024-07-09 20:22:12 +0100

    completed-styling-of-coinSetup-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,11 +33,14 @@ function Navigation() {
 function Body() {
   return (
     <div>
-      <p>Deploy Your Own Token Using</p>
+      <p className={styles.center}>Deploy Your Own Token Using</p>
       <div className={styles.textContainer}>
         <p className={styles.text}>Token Wizard</p>
       </div>
-      <div>Steps</div>
+      <p className={styles.center}>
+        Create your token for your dream project with just few clicks
+      </p>
+      {/* <div>Steps</div> */}
       <Form />
     </div>
   );
@@ -46,43 +49,61 @@ function Body() {
 function Form() {
   return (
     <>
-      <h1>Coin Setup</h1>
+      <h1 className={styles.coinSetup}>Coin Setup</h1>
       <form>
+        <div className={styles.flex}>
+          <label>
+            <p>
+              Token Name <span className={styles.required}>*</span>
+            </p>
+            <input
+              type="text"
+              placeholder="Token Name eg: ETH Token"
+              required
+            />
+          </label>
+          <label>
+            <p>
+              Token Symbol <span className={styles.required}>*</span>
+            </p>
+            <input type="text" placeholder="Token Symbol eg: ETH" required />
+          </label>
+          <label>
+            <p>
+              Total Supply <span className={styles.required}>*</span>
+            </p>
+            <input type="number" placeholder="0" required />
+          </label>
+        </div>
+        <div className={styles.flex}>
+          <label>
+            <p>Telegram</p>
+            <input type="text" placeholder="telegram group link" />
+          </label>
+          <label>
+            <p>Twitter</p>
+            <input type="text" placeholder="twitter.com/accountusername" />
+          </label>
+          <label>
+            <p>Website</p>
+            <input type="text" placeholder="Website URL" />
+          </label>
+        </div>
         <label>
-          <p>Token Name *</p>
-          <input type="text" placeholder="Token Name eg: ETH Token" required />
-        </label>
-        <label>
-          <p>Token Symbol *</p>
-          <input type="text" placeholder="Token Symbol eg: ETH" required />
-        </label>
-        <label>
-          <p>Total Supply *</p>
-          <input type="number" placeholder="0" required />
-        </label>
-        <label>
-          <p>Telegram</p>
-          <input type="text" placeholder="telegram group link" />
-        </label>
-        <label>
-          <p>Twitter</p>
-          <input type="text" placeholder="twitter.com/accountusername" />
-        </label>
-        <label>
-          <p>Website</p>
-          <input type="text" placeholder="Website URL" />
-        </label>
-        <label>
-          <p>Description *</p>
-          <input
-            type="text"
-            placeholder="describe your project to your audience"
+          <p>
+            Description <span className={styles.required}>*</span>
+          </p>
+
+          <textarea
+            rows={5}
+            cols={33}
             required
+            placeholder="describe your project to your audience"
+            className={styles.descriptionBox}
           />
         </label>
-
-        <button>Next</button>
       </form>
+      <button className={styles.button}>Next</button>
     </>
   );
 }
