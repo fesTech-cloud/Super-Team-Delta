7b92e54933d4cc70d2ff06e5854abcc5357a41a6
Ovie Nathaniel <ovienathaniel44@gmail.com>
2024-07-17 12:43:08 +0100

    completed desktop mode

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import toggle from "./toggleSwitch.module.css";
+import formStyle from "./form.module.css";
 
 export function CoinSetup() {
   return (
@@ -71,22 +72,27 @@ export function Distribution() {
           Set up how your token should be initially distributed
         </p>
       </div>
-      <div>
-        <div>
-          <span>Team Allocation</span> <ToggleSwitch label=" " />
+      <div className={formStyle.w50}>
+        <div className={formStyle.container}>
+          <div className={formStyle.flx}>
+            <p>Team&apos;s Allocation</p>
+            <ToggleSwitch label=" " />
+          </div>
+          <form>
+            <label>
+              <p>Amount of tokens for team</p>
+              <input type="number" placeholder="0" />
+            </label>
+          </form>
         </div>
-        <form>
-          <label>
-            <p>Amount of tokens for team</p>
-            <input type="number" placeholder="0" />
-          </label>
-        </form>
-        <form>
-          <label>
-            <p>Team payout address</p>
-            <input type="text" placeholder="" />
-          </label>
-        </form>
+        <div className={formStyle.container}>
+          <form>
+            <label>
+              <p>Team payout address</p>
+              <input type="text" placeholder="" />
+            </label>
+          </form>
+        </div>
       </div>
     </>
   );
@@ -98,25 +104,96 @@ export function BotProtection() {
       <div>
         <h1 className={styles.stepsHeading}>Bot Protection</h1>
         <p style={{ textAlign: "center" }}>
-          Set up how your token should be initially distributed
+          Enable these features if you want to protect your token launch from
+          bots and snipers. You can not change these settings after contract has
+          been renounced
         </p>
       </div>
-      <div>
-        <div>
-          <span>Team Allocation</span> <ToggleSwitch label=" " />
+      <div className={formStyle.w50}>
+        <div className={formStyle.container}>
+          <div className={formStyle.flx}>
+            <p>Limit per wallet</p>
+            <ToggleSwitch label=" " />
+          </div>
+          <form>
+            <label>
+              <p>Max tokens per wallet</p>
+              <input type="number" placeholder="0" />
+            </label>
+          </form>
         </div>
-        <form>
-          <label>
-            <p>Amount of tokens for team</p>
-            <input type="number" placeholder="0" />
-          </label>
-        </form>
-        <form>
-          <label>
-            <p>Team payout address</p>
-            <input type="text" placeholder="" />
-          </label>
-        </form>
+        <div className={formStyle.container}>
+          <div className={formStyle.flx}>
+            <p>Max per transaction</p>
+            <ToggleSwitch label="   " />
+          </div>
+          <form>
+            <label>
+              <p>Max tokens per trade</p>
+              <input type="number" placeholder="0" />
+            </label>
+          </form>
+        </div>
+        <div className={formStyle.container}>
+          <div className={formStyle.flx}>
+            <p>Sniper auto-burn</p>
+            <ToggleSwitch label="  " />
+          </div>
+
+          <p>
+            When you buy in the first block, there&apos;s a 20% extra tax. It
+            decreases to 13.33% in the second block, 6.66% in the third, and no
+            tax from the fourth block onwards. The extra tax money is burned
+            right away, reducing the total supply. Remember, this feature is
+            just to stop snipers, not to make extra money.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function Taxes() {
+  return (
+    <>
+      <div>
+        <h1 className={styles.stepsHeading}>Taxes</h1>
+        <p style={{ textAlign: "center" }}>Enable buy/sell tax on each trade</p>
+      </div>
+
+      <div className={formStyle.w50}>
+        <div className={formStyle.container}>
+          <div className={formStyle.flx}>
+            <p>Buy Tax</p>
+            <ToggleSwitch label="   " />
+          </div>
+          <form>
+            <label>
+              <p>Tax taken for token buy</p>
+              <input type="number" placeholder="0" />
+            </label>
+          </form>
+        </div>
+        <div className={formStyle.container}>
+          <div className={formStyle.flx}>
+            <p>Sell Tax</p>
+            <ToggleSwitch label="    " />
+          </div>
+          <form>
+            <label>
+              <p>Tax taken for token sale</p>
+              <input type="number" placeholder="0" />
+            </label>
+          </form>
+        </div>
+        <div className={formStyle.container}>
+          <form>
+            <label>
+              <p>Tax Payout Address</p>
+              <input type="text" />
+            </label>
+          </form>
+        </div>
       </div>
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 "use client";
 import styles from "./page.module.css";
 import steps from "./steps.module.css";
-import { CoinSetup, Distribution, BotProtection } from "./form";
+import { CoinSetup, Distribution, BotProtection, Taxes } from "./form";
 import { useState } from "react";
 
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
 export default function Home() {
   const [step, setStep] = useState(1);
+  const totalSteps = 4;
+  const progress = ((step - 1) / (totalSteps - 1)) * 100;
 
   function handlePrevious() {
     if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep((s) => s + 1);
+    if (step < 4) setStep((s) => s + 1);
   }
 
   function renderSteps() {
@@ -25,6 +27,8 @@ export default function Home() {
         return <Distribution />;
       case 3:
         return <BotProtection />;
+      case 4:
+        return <Taxes />;
       default:
         return null;
     }
@@ -48,9 +52,35 @@ export default function Home() {
 
       <div className={steps.steps}>
         <div className={steps.numbers}>
+          <div
+            className={`${steps.progress}`}
+            style={{ backgroundColor: "transparent" }}
+          >
+            <div
+              className={steps.progress}
+              style={{
+                height: "4px",
+                background: "#ddd",
+                width: "100%",
+                transition: "all 0.4s ease-in",
+              }}
+            ></div>
+
+            <div
+              className={steps.progress}
+              style={{
+                height: "4px",
+                background: "#7950f2",
+                width: `${progress}%`,
+                transition: "all 0.4s ease-in",
+              }}
+            ></div>
+          </div>
+
           <div className={step >= 1 ? `${steps.active}` : ""}>1</div>
           <div className={step >= 2 ? `${steps.active}` : ""}>2</div>
           <div className={step >= 3 ? `${steps.active}` : ""}>3</div>
+          <div className={step >= 4 ? `${steps.active}` : ""}>4</div>
         </div>
 
         {renderSteps()}
